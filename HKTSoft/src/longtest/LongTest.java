3a8249c92e1d012715ad6c91837de0ae46b5f5bc
Nguyen Trung Long <tieubachlong_52pm1@yahoo.com>
2014-11-13 14:26:58 +0700

    thay doi

--- HKTSoft/src/longtest/LongTest.java
+++ HKTSoft/src/longtest/LongTest.java
@@ -5,9 +5,6 @@
 package longtest;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
-import javax.swing.JOptionPane;
 
 /**
  *
@@ -25,55 +22,53 @@ public class LongTest {
 
     public void run() {
         try {
-            File file = new File(readData() + "\\server\\bin\\webserver.vbs");
-            
-            String cmdarray[] = new String[]{"cmd", "/c",  file.getAbsolutePath()};
-            
-            Runtime.getRuntime().exec(cmdarray);
+            if (CheckOS.isWindows()) {
+                String str = this.getClass().getResource("").getPath().substring(5);
+                File file = new File(str.substring(0, str.indexOf("bin")) + "bin\\pos.bat");
 
-//            File f1 = new File(readData() + "\\server\\bin\\pos.bat");
+                String cmdarray[] = new String[]{"cmd", "/c", file.getAbsolutePath()};
+
+                Runtime.getRuntime().exec(cmdarray);
+                //  File f1 = new File(readData() + "\\server\\bin\\pos.bat");
 //
 //            
 //            String cmdarray[] = new String[]{"cmd", "/c",  f1.getAbsolutePath()};
 //            Runtime.getRuntime().exec(cmdarray);
+            }
+           // JOptionPane.showMessageDialog(null, CheckOS.isUnix());
+            if (CheckOS.isUnix()) {
+                String str = this.getClass().getResource("").getPath().substring(5);
+                
+                File file = new File(str.substring(0, str.indexOf("bin")) + "bin/pos.sh");
+                Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", "chmod +x pos.sh"});
+                Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", file.getPath()});
+            }
+
+
         } catch (Exception e) {
         }
     }
 
-    private String readData() {
-        try {
-            FileInputStream fi = new FileInputStream(getFile("Database", "setup").getAbsoluteFile());
-            ObjectInputStream of = new ObjectInputStream(fi);
-            String str = of.readObject().toString();
-            of.close();
-            return str;
-        } catch (Exception e) {
-            return "";
-
-        }
-
-    }
-
-    public File getFile(String module, String nameFile) {
-        String directory = defaultDirectory() + File.separator
-                + "HKTSoftwareEnterpriseManager" + File.separator + module;
-        if (!new File(directory).exists()) {
-            new File(directory).mkdirs();
-        }
-        String path = directory + File.separator + nameFile;;
-        return new File(path);
-    }
-
-    private String defaultDirectory() {
-        String OS = System.getProperty("os.name").toUpperCase();
-        if (OS.contains("WIN")) {
-            return System.getProperty("user.home");
-        } else if (OS.contains("MAC")) {
-            return System.getProperty("user.home") + "/Library/Application "
-                    + "Support";
-        } else if (OS.contains("NUX")) {
-            return System.getProperty("user.home");
-        }
-        return System.getProperty("user.dir");
-    }
+//    public File getFile(String module, String nameFile) {
+//        String directory = defaultDirectory() + File.separator
+//                + "HKTSoftwareEnterpriseManager" + File.separator + module;
+//        if (!new File(directory).exists()) {
+//            new File(directory).mkdirs();
+//        }
+//        String path = directory + File.separator + nameFile;;
+//        return new File(path);
+//    }
+//
+//    private String defaultDirectory() {
+//        String OS = System.getProperty("os.name").toUpperCase();
+//        if (OS.contains("WIN")) {
+//            return System.getProperty("user.home");
+//        } else if (OS.contains("MAC")) {
+//            return System.getProperty("user.home") + "/Library/Application "
+//                    + "Support";
+//        } else if (OS.contains("NUX")) {
+//            return System.getProperty("user.home");
+//        }
+//        return System.getProperty("user.dir");
+//    }
 }
